517bbf1f71627be6a3573b41257ac7f46e11607d
Jan Brand <P0stbote@muzzy.me>
2015-09-28 18:04:38 +0200

    added some comments

--- qstat_current_players.pl
+++ qstat_current_players.pl
@@ -1,18 +1,17 @@
 #!/usr/bin/perl
-
 $Interval = defined ($ENV{'COLLECTD_INTERVAL'}) ? (0 + $ENV{'COLLECTD_INTERVAL'}) : 120;
 $Hostname = defined ($ENV{'COLLECTD_HOSTNAME'}) ? $ENV{'COLLECTD_HOSTNAME'} : 'localhost';
 $| = 1;
 while(42) {
+	#Set this to your qstat path
 	$qstat_exe='/usr/bin/qstat';
 
-	query_host("a2s", "84.200.19.40", "27015");
-	query_host("a2s", "84.200.19.40", "27016");
-	query_host("a2s", "84.200.19.40", "27017");
+	#Edit the following line to add your gameserver
+	query_host("gametype", "ip", "port");
 	sleep(${Interval});
 }
 
-
+#subroutine for gathering player informations
 sub query_host {
 	$gametype=$_[0];
 	$ip=$_[1];
