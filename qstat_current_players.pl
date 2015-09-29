41f3772e2b62c296bc24f97b5428b1e69731c7dd
Jan <uffsalot@users.noreply.github.com>
2015-09-29 15:12:43 +0200

    Use strict & warnings

--- qstat_current_players.pl
+++ qstat_current_players.pl
@@ -1,32 +1,36 @@
 #!/usr/bin/perl
-$Interval = defined ($ENV{'COLLECTD_INTERVAL'}) ? (0 + $ENV{'COLLECTD_INTERVAL'}) : 120;
-$Hostname = defined ($ENV{'COLLECTD_HOSTNAME'}) ? $ENV{'COLLECTD_HOSTNAME'} : 'localhost';
+use strict;
+use warnings;
+
+my $Interval = defined ($ENV{'COLLECTD_INTERVAL'}) ? (0 + $ENV{'COLLECTD_INTERVAL'}) : 120;
+my $Hostname = defined ($ENV{'COLLECTD_HOSTNAME'}) ? $ENV{'COLLECTD_HOSTNAME'} : 'localhost';
 $| = 1;
 
 #Set this to your qstat path
-$qstat_exe='/usr/bin/qstat';
+my $qstat_exe='/usr/bin/qstat';
 if (-e $qstat_exe) {
 	while(42) {
 		#Edit the following line to add your gameserver
-		query_host("gametype", "ip", "port");
+		query_host("a2s", "84.200.19.40", "27015");
 		sleep(${Interval});
 	}
 }else{
 	print "Please install qstat or modify the path to the qstat binary.";
 }
+
 #subroutine for gathering player informations
 sub query_host {
-	$gametype=$_[0];
-	$ip=$_[1];
-	$port=$_[2];
+	my $gametype=$_[0];
+	my $ip=$_[1];
+	my $port=$_[2];
 
-	$raw= `${qstat_exe} -raw ';' -nh -${gametype} ${ip}:${port}`;
-	@result = split /;/, $raw;
+	my $raw= `${qstat_exe} -raw ';' -nh -${gametype} ${ip}:${port}`;
+	my @result = split /;/, $raw;
 	if($result[2] !~ /DOWN/)
 
 	{
-		($host, $max_players, $players) = @result[1,4,5];
-		$time = time;
+		my($host, $max_players, $players) = @result[1,4,5];
+		my $time = time;
 		print "PUTVAL ${Hostname}/${gametype}_${ip}_${port}/gauge-players interval=${Interval} ${time}:${players}\n";
 		print "PUTVAL ${Hostname}/${gametype}_${ip}_${port}/gauge-max_players interval=${Interval} ${time}:${max_players}\n";
 	}
